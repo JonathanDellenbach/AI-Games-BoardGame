3e4ff406105085a65d844f131cc9d8c79b89c511
JonathanDellenbach <C00281735@itcarlow.ie>
2025-11-14 00:21:57 +0000

    Added Piece Placing

--- BoardGame/BoardGame/src/Board.cpp
+++ BoardGame/BoardGame/src/Board.cpp
@@ -105,3 +105,29 @@ sf::RectangleShape* Board::getGameBoardCell(int col, int row)
     }
     return nullptr;
 }
+
+GridPos Board::screenToSelectionGrid(int mouseX, int mouseY) const //convert pixel coord to selection grid pos
+{
+    int col = static_cast<int>((mouseX - GRID_OFFSET_X) / CELL_SIZE);
+    int row = static_cast<int>((mouseY - GRID_OFFSET_Y) / CELL_SIZE);
+    return GridPos{ col, row };
+}
+
+GridPos Board::screenToGameBoard(int mouseX, int mouseY) const // convert pixel coord to board grid pos
+{
+    int col = static_cast<int>((mouseX - BOARD_OFFSET_X) / CELL_SIZE);
+    int row = static_cast<int>((mouseY - GRID_OFFSET_Y) / CELL_SIZE);
+    return GridPos{ col, row };
+}
+
+bool Board::isInSelectionGrid(int mouseX, int mouseY) const // ensures the click its inside of the selection grid
+{
+    GridPos pos = screenToSelectionGrid(mouseX, mouseY);
+    return (pos.x >= 0 && pos.x < 2 && pos.y >= 0 && pos.y < 5);
+}
+
+bool Board::isInGameBoard(int mouseX, int mouseY) const // ensures the click its inside of the boaord grid
+{
+    GridPos pos = screenToGameBoard(mouseX, mouseY);
+    return (pos.x >= 0 && pos.x < 5 && pos.y >= 0 && pos.y < 5);
+}
